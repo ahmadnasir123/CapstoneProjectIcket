48f0446336b6a3c95409c44ec621f2b4609586ba
Nasir <nasirahmad240502@gmail.com>
2023-06-10 22:20:32 +0700

    init commit

--- app/src/main/java/com/C23PS326/icket/ui/components/BudayaItem.kt
+++ app/src/main/java/com/C23PS326/icket/ui/components/BudayaItem.kt
@@ -10,15 +10,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.res.stringResource
-import com.C23PS326.icket.model.Category
+import coil.compose.AsyncImage
 import com.C23PS326.icket.model.Rekomendasi
 import com.C23PS326.icket.R
 
@@ -60,11 +58,19 @@ fun BudayaItem(
 
 @Composable
 @Preview(showBackground = true)
-fun MenuItemPreview() {
+fun BudayaItemPreview() {
     MaterialTheme {
         BudayaItem(
-            budaya = Category(R.drawable.menu2, "Hot Pumpkin Spice Latte Premium", "Rp 18.000"),
-            modifier = Modifier.padding(8.dp)
+            budaya =  Rekomendasi(
+                1,
+                "Wayang Kulit",
+                "Wayang kulit merupakan seni tradisional Indonesia yang lahir, hidup, dan berkembang terutama di masyarakat Jawa. Dalam pertunjukannya, wayang kulit bukan hanya sekadar hiburan, tetapi juga menyimpan nilai spiritual dan filosofis yang mendalam. Melalui media kulit kerbau, seorang dalang memainkan karakter-karakter dalam cerita wayang, disertai dengan musik gamelan dan nyanyian pesinden.\n" +
+                        "\n" +
+                        "Setiap bagian pementasan memiliki simbol dan makna filosofis yang kuat, sambil mengajarkan budi pekerti yang luhur serta menyampaikan pesan sosial melalui adegan-adegan yang lucu. Wayang kulit, sebagai embrio dari berbagai jenis wayang lainnya, memiliki sejarah yang panjang dan telah menyebar ke berbagai wilayah di Indonesia. Dengan keunikan dan kekayaannya, wayang kulit diakui sebagai warisan budaya dunia oleh UNESCO.\n",
+                "https://asset.kompas.com/crops/LeDT4hIHS0zJ5Bq7Yu6dhLp_ysM=/16x0:691x450/750x500/data/photo/2022/09/09/631af8ce3c329.jpg"
+            ),
+
+                    modifier = Modifier.padding(8.dp)
         )
     }
 }
