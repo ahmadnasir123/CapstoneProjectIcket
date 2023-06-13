b995d9e53b4e280bd2020aec87c2a0ff3b860e0d
Ali <aldiansyahali717@gmail.com>
2023-06-14 02:06:06 +0700

    init commit

---
+++ app/src/main/java/com/C23PS326/icket/model/provinsi/DataProvinsi.kt
@@ -0,0 +1,55 @@
+package com.C23PS326.icket.model.provinsi
+
+
+object DataProvinsi {
+    val dataprovinsi = listOf(
+        Provinsi(
+            1,
+            "Papua Barat Daya",
+        "Sorong",
+        "Papua Barat Daya adalah sebuah provinsi di Indonesia, dan merupakan pemekaran dari Papua Barat. Meski bernama Barat Daya, namun provinsi ini terletak di bagian barat laut Pulau Papua, sebuah contoh misnomer."
+        ),
+        Provinsi(
+            2,
+            "Papua Selatan",
+            "Merauke",
+            "Papua Selatan adalah salah satu provinsi di Indonesia yang telah dimekarkan dari Provinsi Papua. Ibu kotanya berada di Kabupaten Merauke, tepatnya di Kota Terpadu Mandiri Salor di Distrik Kurik sekitar 60 km dari Kota Merauke"
+        ),
+        Provinsi(
+            3,
+            "Papua Pegunungan",
+            "Sorong",
+            "Papua Pegunungan adalah sebuah provinsi di Indonesia yang dimekarkan dari provinsi Papua pada sidang Paripurna DPR RI pada tanggal 30 Juni 2022. Ibu kotanya berkedudukan di Kabupaten Jayawijaya."
+        ),
+        Provinsi(
+            4,
+            "Papua Tengah",
+            "Nabire",
+            "Papua Tengah adalah sebuah provinsi di Indonesia yang telah dimekarkan dari provinsi Papua pada tahun 2022.Ibukota Provinsi ini berada di Kabupaten Nabire.Papua Tengah dimekarkan dari Provinsi Papua bersama dua provinsi lainnya yakni Papua Pegunungan dan Papua Selatan pada 30 Juni 2022 berdasarkan Undang-Undang Nomor 15 Tahun 2022.Cakupan wilayah Papua Tengah kira-kira sesuai dengan wilayah adat Mee Pago dan Saireri. Kabupaten Nabire di bagian utara Papua Tengah merupakan dataran rendah berbatasan langsung dengan Taman Nasional Teluk Cenderawasih yang memiliki potensi pariwisata bahari seperti terumbu karang, pulau-pulau berpasir putih dan hiu paus.Bagian tengah Papua Tengah terdapat kawasan Danau Paniai dan Pegunungan Jayawijaya. Di provinsi ini terdapat gunung tertinggi di Indonesia yaitu Puncak Jaya serta tambang emas Grasberg yang dioperasikan oleh Freeport Indonesia.Bagian selatan Papua Tengah adalah Kabupaten Mimika dengan ibu kotanya Timika yang merupakan salah satu kota besar di Pulau Papua. Sedangkan topografi Mimika berupa rawa-rawa, sungai, dan pantai."
+        ),
+        Provinsi(
+            5,
+            "Papua Barat",
+            "Manokwari",
+            "Provinsi Papua Barat adalah sebuah provinsi di Indonesia. Ibu kota provinsi ini adalah Kota Manokwari. Provinsi Papua Barat terletak di bagian barat Pulau Papua dan berbatasan dengan Provinsi Papua di sebelah timur. Papua Barat memiliki luas wilayah sekitar 114.566 kilometer persegi dan merupakan provinsi terbesar kedua di Indonesia setelah Provinsi Papua."
+        ),
+        Provinsi(
+            6,
+            "Sulawesi Barat",
+            "Mamuju",
+            "Papua Barat Daya adalah sebuah provinsi di Indonesia, dan merupakan pemekaran dari Papua Barat. Meski bernama Barat Daya, namun provinsi ini terletak di bagian barat laut Pulau Papua, sebuah contoh misnomer."
+        ),
+        Provinsi(
+            7,
+            "Kepulauan Riau",
+            "Tanjung Pinang",
+            "Papua Barat Daya adalah sebuah provinsi di Indonesia, dan merupakan pemekaran dari Papua Barat. Meski bernama Barat Daya, namun provinsi ini terletak di bagian barat laut Pulau Papua, sebuah contoh misnomer."
+        ),
+        Provinsi(
+            8,
+            "Gorontalo",
+            "Gorontalo",
+            "Papua Barat Daya adalah sebuah provinsi di Indonesia, dan merupakan pemekaran dari Papua Barat. Meski bernama Barat Daya, namun provinsi ini terletak di bagian barat laut Pulau Papua, sebuah contoh misnomer."
+        ),
+    )
+}
---
+++ app/src/main/java/com/C23PS326/icket/model/provinsi/Provinsi.kt
@@ -0,0 +1,8 @@
+package com.C23PS326.icket.model.provinsi
+
+data class Provinsi(
+    val provinsiId: Long,
+    val name: String,
+    val ibukota: String,
+    val desc: String,
+)
